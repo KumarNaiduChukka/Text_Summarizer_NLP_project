c0b22ae658f19c27eca4230606e9815237703853
KumarNaiduChukka <chukkakumarnaidu2003@gmail.com>
2024-06-08 11:00:57 +0530

    requirements & installation done

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+    
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "Text_Summarizer_NLP_project"
+AUTHOR_USER_NAME = "KumarNaiduChukka"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "chukkakumarnaidu2003@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    auther=AUTHOR_USER_NAME,
+    auther_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls ={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
+
+    
